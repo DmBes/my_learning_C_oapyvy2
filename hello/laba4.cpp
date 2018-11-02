0b760ab047e0faf70a2db96d898b616ebafa243d
Dmitry <1@eml.by>
2018-11-03 00:49:37 +0300

    дорешал со вторым изменениям

--- hello/laba4.cpp
+++ hello/laba4.cpp
@@ -10,7 +10,7 @@
 void my_print(int, double*);
 void inputArray(int, double*, char*);
 
-void main()
+void main1()
 {
 	setlocale(LC_CTYPE, "rus");
 	system("cls");
@@ -56,6 +56,8 @@ void main()
 		my_print(user_M, massivM);
 	}
 	else printf_s("Сумма массивов N, M РАВНА и составляет %lf", massivN[user_N]);
+	free(massivM);
+	free(massivN);
 
 	_getch();
 }
@@ -68,7 +70,7 @@ void inputArray(int Cikl, double *array, char *nomer_massiva)
 	double summ = 0;
 	for (int begin = 0; begin < Cikl; begin++)
 	{
-		printf_s("\nВведите значения массива [%d],[%d]= ", *nomer_massiva, begin);
+		printf_s("\nВведите значения массива [%c],[%d]= ", *nomer_massiva, begin+1);
 		scanf_s("%lf", &array[begin]);
 		//printf_s("%.4lf", array[begin]);
 		begin == 0 ? summ = array[begin] : summ = summ * array[begin];
@@ -77,7 +79,9 @@ void inputArray(int Cikl, double *array, char *nomer_massiva)
 
 	}
 	array[Cikl] = summ;
-	//nomer_massiva++;
+	//char replece_name[] = "g";
+	//nomer_massiva = replece_name;
+
 }
 
 
@@ -88,4 +92,78 @@ void my_print(int Cikl, double *array)
 		printf_s("Значение %d массива составлют : %.4lf\n", k+1, array[k]);
 	}
 	printf_s("Спасибо за просмотр!!!!!");
+}
+
+
+
+
+void my_print2(double *array_1, double *array_2, int a, int b)
+{
+	printf_s("Массив 1 равен %lf, Массив 2 равен %lf\n", array_1[0], array_2[0]);
+	if (array_1[0] == array_2[0]) {
+		printf_s("Значения диапазонов равны");
+	}
+	else if (array_1[0] < array_2[0]) 
+	{
+		for (int k = 1; k < a + 1; k++)
+		{
+			printf_s("Значение %d массива составлют : %.4lf\n", k , array_1[k]);
+		}
+		printf_s("Спасибо за просмотр!!!!!");
+	}
+	else
+	{
+		for (int k = 1; k < a + 1; k++)
+		{
+			printf_s("Значение %d массива составлют : %.4lf\n", k , array_2[k]);
+		}
+		printf_s("Спасибо за просмотр!!!!!");
+	}
+	
+}
+
+
+
+
+void function2(int, double*, char *user_name_array);
+
+
+void main()
+{
+	setlocale(LC_CTYPE, "rus");
+	system("cls");
+	int N, M;
+	char nomer[] = "N";
+	char *nomer_massive = nomer;
+
+
+	printf("Введите размер массива A: ");
+	scanf_s("%d", &N);
+	printf("Введите размер массива B: ");
+	scanf_s("%d", &M);
+	double *massivA = (double *)(calloc((N+1),sizeof(double)));
+	double *massivB = (double *)(calloc((M+1), sizeof(double)));
+	function2(N, massivA, nomer_massive);
+	function2(M, massivB, nomer_massive);
+	my_print2(massivA, massivB, N, M);
+	_getch();
+}
+
+
+void function2(int user_num, double *user_mass, char *user_name_array) 
+{
+	double multiplicatin = 0;
+
+		for (int step = 1; step < user_num + 1; step++)
+		{
+			printf_s("Введите число %d массива %c : ", step, *user_name_array);
+			scanf_s("%lf", &user_mass[step]);
+			step == 1 ? multiplicatin = user_mass[step] : multiplicatin = user_mass[step] * user_mass[0];
+			user_mass[0] = multiplicatin;
+			//printf_s("\n%lf", user_mass[0]);
+
+		}
+
+
+
 }
