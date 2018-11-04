f6d5ff79daf8b1aa40ea30db0304a7214cc7328c
Dmitry <1@eml.by>
2018-11-05 00:09:14 +0300

    done laba 5-1;

--- hello/laba4.cpp
+++ hello/laba4.cpp
@@ -10,13 +10,13 @@
 void my_print(int, double*);
 void inputArray(int, double*, char*);
 
-void main1()
+void main3()
 {
 	setlocale(LC_CTYPE, "rus");
 	system("cls");
-	
+
 	int user_N, user_M;
-	char nomer[] = "N";
+	char nomer[] = "a";
 	char *nomer_massive = nomer;
 	double *massivN; //Создание массивов N и М
 	double *massivM;
@@ -24,19 +24,19 @@ void main1()
 	scanf_s("%d", &user_N);
 	printf("Введите размер массива M: ");
 	scanf_s("%d", &user_M);
-	massivN = (double*)(calloc((user_N + 1) , sizeof(double)));// выделяем память под массив N
-	massivM = (double*)(calloc((user_M + 1) , sizeof(double)));// Выделяем память под массив M
-	
-	//double summ = 0;
-	//for (begin = 0; begin < user_N; begin++)
-	//{
-	//	printf_s("\nВведите значения массива [%d],[%d]= ", nomer, begin);
-	//	scanf_s("%lf", &massivN[begin]);//Может нужно убрать &
-	//	printf_s("%.4lf", massivN[begin]);
-	//	summ += massivN[begin];
-	//	printf_s("\n%lf", summ);
+	massivN = (double*)(calloc((user_N + 1), sizeof(double)));// выделяем память под массив N
+	massivM = (double*)(calloc((user_M + 1), sizeof(double)));// Выделяем память под массив M
 
-	//}
+															  //double summ = 0;
+															  //for (begin = 0; begin < user_N; begin++)
+															  //{
+															  //	printf_s("\nВведите значения массива [%d],[%d]= ", nomer, begin);
+															  //	scanf_s("%lf", &massivN[begin]);//Может нужно убрать &
+															  //	printf_s("%.4lf", massivN[begin]);
+															  //	summ += massivN[begin];
+															  //	printf_s("\n%lf", summ);
+
+															  //}
 
 	inputArray(user_N, massivN, nomer);
 	inputArray(user_M, massivM, nomer);
@@ -70,7 +70,7 @@ void inputArray(int Cikl, double *array, char *nomer_massiva)
 	double summ = 0;
 	for (int begin = 0; begin < Cikl; begin++)
 	{
-		printf_s("\nВведите значения массива [%c],[%d]= ", *nomer_massiva, begin+1);
+		printf_s("\nВведите значения массива [%c],[%d]= ", *nomer_massiva, begin + 1);
 		scanf_s("%lf", &array[begin]);
 		//printf_s("%.4lf", array[begin]);
 		begin == 0 ? summ = array[begin] : summ = summ * array[begin];
@@ -79,17 +79,17 @@ void inputArray(int Cikl, double *array, char *nomer_massiva)
 
 	}
 	array[Cikl] = summ;
-	//char replece_name[] = "g";
-	//nomer_massiva = replece_name;
+	char replece_name = 'b';
+	*nomer_massiva = replece_name;
 
 }
 
 
-void my_print(int Cikl, double *array) 
+void my_print(int Cikl, double *array)
 {
-	for (int k = 0; k < Cikl; k++) 
+	for (int k = 0; k < Cikl; k++)
 	{
-		printf_s("Значение %d массива составлют : %.4lf\n", k+1, array[k]);
+		printf_s("Значение %d массива составлют : %.4lf\n", k + 1, array[k]);
 	}
 	printf_s("Спасибо за просмотр!!!!!");
 }
@@ -103,11 +103,11 @@ void my_print2(double *array_1, double *array_2, int a, int b)
 	if (array_1[0] == array_2[0]) {
 		printf_s("Значения диапазонов равны");
 	}
-	else if (array_1[0] < array_2[0]) 
+	else if (array_1[0] < array_2[0])
 	{
 		for (int k = 1; k < a + 1; k++)
 		{
-			printf_s("Значение %d массива составлют : %.4lf\n", k , array_1[k]);
+			printf_s("Значение %d массива составлют : %.4lf\n", k, array_1[k]);
 		}
 		printf_s("Спасибо за просмотр!!!!!");
 	}
@@ -115,11 +115,11 @@ void my_print2(double *array_1, double *array_2, int a, int b)
 	{
 		for (int k = 1; k < a + 1; k++)
 		{
-			printf_s("Значение %d массива составлют : %.4lf\n", k , array_2[k]);
+			printf_s("Значение %d массива составлют : %.4lf\n", k, array_2[k]);
 		}
 		printf_s("Спасибо за просмотр!!!!!");
 	}
-	
+
 }
 
 
@@ -128,12 +128,12 @@ void my_print2(double *array_1, double *array_2, int a, int b)
 void function2(int, double*, char *user_name_array);
 
 
-void main()
+void main22()
 {
 	setlocale(LC_CTYPE, "rus");
 	system("cls");
 	int N, M;
-	char nomer[] = "N";
+	char nomer[] = "A";
 	char *nomer_massive = nomer;
 
 
@@ -141,10 +141,10 @@ void main()
 	scanf_s("%d", &N);
 	printf("Введите размер массива B: ");
 	scanf_s("%d", &M);
-	double *massivA = (double *)(calloc((N+1),sizeof(double)));
-	double *massivB = (double *)(calloc((M+1), sizeof(double)));
-	function2(N, massivA, nomer_massive);
-	function2(M, massivB, nomer_massive);
+	double *massivA = (double *)(calloc((N + 1), sizeof(double)));
+	double *massivB = (double *)(calloc((M + 1), sizeof(double)));
+	function2(N, massivA, nomer);
+	function2(M, massivB, nomer);
 	my_print2(massivA, massivB, N, M);
 	free(massivA);
 	free(massivB);
@@ -152,20 +152,106 @@ void main()
 }
 
 
-void function2(int user_num, double *user_mass, char *user_name_array) 
+void function2(int user_num, double *user_mass, char *user_name_array)
 {
 	double multiplicatin = 0;
 
-		for (int step = 1; step < user_num + 1; step++)
+	for (int step = 1; step < user_num + 1; step++)
+	{
+		printf_s("Введите число %d массива %c : ", step, *user_name_array);
+		scanf_s("%lf", &user_mass[step]);
+		step == 1 ? multiplicatin = user_mass[step] : multiplicatin = user_mass[step] * user_mass[0];
+		user_mass[0] = multiplicatin;
+		//printf_s("\n%lf", user_mass[0]);
+
+	}
+
+	char replece_name = 'B';
+	*user_name_array = replece_name;
+
+}
+
+
+
+
+
+void zadanie2(int razmer_massiva, int *sam_massiv)
+{
+	int summa = 0;
+	for (razmer_massiva; razmer_massiva >= 0; razmer_massiva--)
+	{
+		if (sam_massiv[razmer_massiva] != 0 && razmer_massiva == 0)
 		{
-			printf_s("Введите число %d массива %c : ", step, *user_name_array);
-			scanf_s("%lf", &user_mass[step]);
-			step == 1 ? multiplicatin = user_mass[step] : multiplicatin = user_mass[step] * user_mass[0];
-			user_mass[0] = multiplicatin;
-			//printf_s("\n%lf", user_mass[0]);
+			printf_s("Функция не имеет решения");
+		}
+		else if (sam_massiv[razmer_massiva] != 0)
+		{
+			continue;
+		}
+		else
+		{
+			for (razmer_massiva; razmer_massiva >= 0; razmer_massiva--)
+			{
+				summa += sam_massiv[razmer_massiva];
+			}
+			printf_s("Сумма диапазона равна %d", summa);
+			_getch();
+			break;
+		}
+	}
+
+}
+
+
+
+void main5()
+{
+	int n;
+	int *array;
+	setlocale(LC_CTYPE, "rus");
+	system("cls");
+
+	printf_s("Введите размер массива A: ");
+	scanf_s("%d", &n);
+	array = new int[n];
+	if (!array) {
+		printf("Out of memory. Press any key: ");
+		_getch();
+		exit(1);
+	}
+
+	int number = 0;
+	int stepik = 0;
+
+	while (stepik < n)
+	{
+		printf_s("Введите число %d массива: ", stepik + 1);
+		scanf_s("%d", &number);
+		if (number == 0)
+		{
+			stepik++;
+			break;
+		}
+		else if (stepik == n)
+		{
 
 		}
+		else {
+			stepik++;
+			if (stepik == n && number != 0) {
+				printf_s("Функция не имеет решения\n");
+				break;
+			}
+		}
+	}
 
 
-
+	for (stepik + 1; stepik < n; stepik++, number++)
+	{
+		printf_s("Введите число %d массива: ", stepik + 1);
+		scanf_s("%d", &array[number]);
+		//printf_s("Вывод массива %d\n", array[number]);
+	}
+	zadanie2(number, array);
+	_getch();
 }
--- hello/laba5.cpp
+++ hello/laba5.cpp
@@ -5,23 +5,79 @@
 #include <stdlib.h>
 
 
+void fill_array(int **, int, int);
+int max(int **, int, int);
+
+
 
 void main()
 {
 	setlocale(LC_CTYPE, "Rus");
 	system("cls");
-	int size_array, wide_array;
+	int size_array, wide_array, max_ch;
 	int **array;
-	printf_s("¬ведите длинну массива: ");
+m3: printf_s("¬ведите длинну массива: ");
 	scanf_s("%d", &size_array);
-	printf_s("¬ведите ширину массива: ");
-	scanf_s("%d", wide_array);
-	array = (int **)malloc(sizeof(int*)* (size_array));
-	for (int i = 0; i < wide_array; i++)
+	if (size_array < 1) {
+		printf_s("¬ведите число больше 0\n");
+		goto m3;
+	}
+m1: printf_s("¬ведите ширину массива: ");
+	scanf_s("%d", &wide_array);
+	if (wide_array < 1) {
+		printf_s("¬ведите число больше 0\n");
+		goto m1;
+	}
+	system("cls");
+	array = new int*[size_array];
+	for (int begin_array = 0; begin_array < size_array; begin_array++)
 	{
-		(*(array + i) = (int*)malloc(sizeof(int)*wide_array));
+		*(array + begin_array) = new int[wide_array];
+		//for (int filling_array = 0; filling_array < wide_array; filling_array++) 
+		//{
+		//	printf_s("¬ведите в строке %d, число %d массива ", begin_array, filling_array);
+		//	scanf_s("%d", &array[begin_array][filling_array]);
+		//	printf_s("¬аше число равно : %d\n", array[begin_array][filling_array]);
+		//}
+	}
+	fill_array(array, size_array, wide_array);
+	max_ch = max(array, size_array, wide_array);
+	printf_s("ћаксимальное число в массиве равно %d", max_ch);
+	_getch();
 	}
 
 
+void fill_array(int **massive, int string, int width)
+{
+	for (int i = 0; i < string; i++)
+	{
+		for (int k = 0; k < width; k++)
+		{
+			printf_s("¬ведите в строке %d, число %d массива ", i+1, k+1);
+			scanf_s("%d", &massive[i][k]);
+//			printf_s("¬аше число равно : %d\n", *(*(massive + i) + k));
+		}
+	}
+//	printf_s("Hello");
+}
 
+
+int max(int **mass, int stroka, int visota)
+{
+	int max_number;
+	max_number = mass[0][0];
+
+	for (int i = 0; i < stroka; i++)
+	{
+		for (int k = 0; k < visota; k++)
+		{
+			if (max_number > *(*(mass + i) + k)) { continue; }
+			else
+			{
+				max_number = *(*(mass + i) + k);
+			} 
+
+		}
+	}
+	return max_number;
 }
